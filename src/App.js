6d6f7944edeca88be46c232499d312e975fa9dfc
Nishad Umap <umapnishad1@gmail.com>
2023-10-05 07:48:25 +0530

    login page is done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Login from './components/login/Login'
+function App() {
+  return (
+    <div className='container'>
+     <Login  />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,100 @@
+import React, { useState ,useEffect} from "react";
+import "./login.css";
+import side_img from "../Assests/image 13.png";
+const Login = () => {
+  const initialValues ={Name:"",UserName:"",Email:"",Mobile:""}
+  const [formValues,setFormValues]=useState(initialValues)
+  const [formErrors,setFormErrors]=useState({})
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [TnC,setTnC]=useState(false);
+
+  const handleChange=(e)=>{
+    const {name , value}=e.target;
+    setFormValues({...formValues,[name]:value});
+  };
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+    setTnC(true)
+  };
+  
+  useEffect(()=>{
+    console.log(formErrors)
+    if(Object.keys(formErrors).length===0 && isSubmit && setTnC){
+      console.log(formValues);
+    }
+  },[formErrors]);
+
+  const validate = (values) => 
+  {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.name) {
+      errors.name = "Field is required";
+    }
+    if (!values.username) {
+      errors.username = "Field is required";
+    }else if(values.username.length <5 ){
+      errors.username = "UserName Must be atleast 5 Characers";
+    }else if(values.username.length >15 ){
+      errors.username = "UserName Must not excced 15 Characers";
+    }
+
+    if (!values.email) {
+      errors.email = "Field is required";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+
+    if (!values.mobile) {
+      errors.mobile = "Field is required";
+    }else if(values.mobile.length >10 ){
+      errors.mobile = "Mobile No must be 10 Digits";
+    }else if(values.mobile.length <10 ){
+      errors.mobile = "Mobile No must be 10 Digits";
+    }else if (values.Mobile.length === 10) {
+      errors.mobile = "Mobile No must be 10 Digits";
+    }
+  
+    if(!values.c_box){errors.c_box="Check this box if you want to proceed";}
+    
+    return errors;
+  };
+  return (
+  <div className="page_1">
+      <div className="image_bg">
+        <img src={side_img} alt="image_isde" className="image_bg" />
+      </div>
+      <div className="login_form">
+        <h2 className="heading">Supper App</h2>
+        <h4 className="heading2">Create your new account</h4>
+      <form onSubmit={handleSubmit}>
+
+          <input value={formValues.name} className="form_input" id="name" placeholder="Name" name="name" type="text" onChange={handleChange}></input>
+          <p className="errormsg">{formErrors.name}</p>
+
+          <input className="form_input" id="username" placeholder="UserName" name="username" type="text" value={formValues.username} onChange={handleChange}></input>
+          <p className="errormsg">{formErrors.username}</p>
+
+          <input className="form_input" id="email" placeholder="Email" name="email" type="email" value={formValues.email} onChange={handleChange}></input>
+          <p className="errormsg">{formErrors.email}</p>
+
+          <input className="form_input" id="mobile" placeholder="Mobile" name="mobile" type="number" value={formValues.mobile} onChange={handleChange} maxLength="10" ></input>
+          <p className="errormsg">{formErrors.mobile}</p>
+            
+          
+           <label className="c_box"> 
+            <input type="checkbox" onChange={handleChange} name="c_box"/>Share my registration data with Superapp</label>
+            <p className="errormsg">{formErrors.c_box}</p>
+          <button className="s_button">SIGN UP</button>
+      </form>
+        <span className="p1">By clicking on Sign up. you agree to Superapp <p className="terms_codN">Terms and Conditions of Use</p></span>
+        <span className="p2">To learn more about how Superapp collects, uses, shares and protects your personal data please head Superapp</span>
+          <p className="terms_codN">Terms and Conditions of Use</p>
+      </div>
+    </div>
+  );
+};
+export default Login;
